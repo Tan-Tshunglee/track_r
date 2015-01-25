390264f6af6dd0d61f00302ec60a9ce8a08e1c1b
Tshunglee <tanchangli111@126.com>
2015-01-25 12:07:50 +0800

    send password task start

--- app/src/main/java/com/antilost/app/network/SendPassowdCommand.java
+++ app/src/main/java/com/antilost/app/network/SendPassowdCommand.java
@@ -1,15 +1,25 @@
 package com.antilost.app.network;
 
+import android.util.Log;
+
 /**
  * Created by Tan on 2015/1/24.
  */
 public class SendPassowdCommand extends Command {
 
-    public SendPassowdCommand(String email) {
+    private static final String LOG_TAG = "SendPassowdCommand";
+    private final String mEmail;
 
+    public SendPassowdCommand(String email) {
+        mEmail = email;
     }
     @Override
     protected String makeRequestString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("cmd:getpasswd").append(LINE_SPLITTER)
+                .append("uid:") .append(mEmail).append(LINE_SPLITTER);
+        String result = sb.toString();
+        Log.v(LOG_TAG, "request string is " + result);
+        return result;
     }
 }
