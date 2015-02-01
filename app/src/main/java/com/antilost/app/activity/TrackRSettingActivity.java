9bf4c0e7eda43e5fc8036001d35d1382cd8f766e
Tshunglee <tanchangli111@126.com>
2015-02-01 18:15:46 +0800

    track r setting page start

---
+++ app/src/main/java/com/antilost/app/activity/TrackRSettingActivity.java
@@ -0,0 +1,17 @@
+package com.antilost.app.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.antilost.app.R;
+
+public class TrackRSettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_rsetting);
+    }
+
+
+}
