f8d1e4e7395cfe33e41abf6b7b5db598e803a6d9
zqinglin <zqinglin1102@163.com>
2015-03-08 17:36:36 +0800

    add alartimesetting view

--- app/src/main/java/com/antilost/app/activity/UserProfileActivity.java
+++ app/src/main/java/com/antilost/app/activity/UserProfileActivity.java
@@ -41,6 +41,7 @@ public class UserProfileActivity extends Activity implements TrackRInitialize, C
     private TrackRDataBase trackRDataBase;
     /** 数据库对象 */
     private SQLiteDatabase mDb = null;
+    public final String AlartTime = "AlartTime";
 
     private UserdataBean   curUserDataBean;
 
@@ -78,8 +79,6 @@ public class UserProfileActivity extends Activity implements TrackRInitialize, C
             curUserDataBean = UserDataTable.getInstance().query(mDb);
             //setting icon
             if(curUserDataBean.getMimage()!="3"){
-
-
                 int targetWidth = 100;
                 int targetHeight = 100;
                 Bitmap targetBitmap = Bitmap.createBitmap(
@@ -103,7 +102,7 @@ public class UserProfileActivity extends Activity implements TrackRInitialize, C
 
                 imgUser_usericon.setImageBitmap(targetBitmap);
             }
-            tvtime.setText(curUserDataBean.getMalarmtime()+"S");
+            tvtime.setText(curUserDataBean.getMalarmtime()+getResources().getString(R.string.alarttime_second));
 
             if(curUserDataBean==null){
                 Log.d(TAG," curUserDataBean==null");
@@ -187,6 +186,19 @@ public class UserProfileActivity extends Activity implements TrackRInitialize, C
                 // TODO Auto-generated method stub
 //                Intent intent = new Intent(UserProfileActivity.this, MainTrackRListActivity.class);
 //                startActivity(intent);
+                UserProfileActivity.this.finish();
+
+            }
+        });
+
+        rluser_noticetimer.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(UserProfileActivity.this, AlartTime.class);
+                intent.putExtra(AlartTime,curUserDataBean.getMalarmtime());
+                startActivity(intent);
                 UserProfileActivity.this.finish();
 
             }
