af4c16e3f5f37cebf4495d50ab0b32ab5efc66ee
zqinglin <zqinglin1102@163.com>
2015-03-08 17:54:03 +0800

    Revert "fdadafdfRevert "add alartimesetting view""
    This reverts commit af63a820a7e8adbf87cdbcd856245c7dabc8fca6.

---
+++ app/src/main/java/com/antilost/app/activity/AlartTime.java
@@ -0,0 +1,181 @@
+package com.antilost.app.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.antilost.app.R;
+import com.antilost.app.common.TrackRInitialize;
+import com.antilost.app.dao.TrackRDataBase;
+import com.antilost.app.dao.UserDataTable;
+import com.antilost.app.model.UserdataBean;
+import com.antilost.app.util.CsstSHImageData;
+import com.antilost.app.util.wheelview.NumericWheelAdapter;
+import com.antilost.app.util.wheelview.OnWheelChangedListener;
+import com.antilost.app.util.wheelview.OnWheelScrollListener;
+import com.antilost.app.util.wheelview.WheelView;
+
+import java.io.File;
+import java.util.Calendar;
+
+public class AlartTime extends Activity implements TrackRInitialize {
+    private String TAG = "AlartTime";
+    private Button btmBack, btmDone;
+    private TextView tvtitle ;
+    private TextView tvAlarttime;
+    private WheelView wvAlarTime;
+    private int  ialarttime = 0;
+    public final String AlartTime = "AlartTime";
+    /** �������*/
+    //数据库
+    private TrackRDataBase trackRDataBase;
+    /** 数据库对象 */
+    private SQLiteDatabase mDb = null;
+
+    private UserdataBean   curUserDataBean;
+
+    private boolean timeScrolled = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.alarttime);
+        initWidget();
+        initWidgetState();
+        initWidgetListener();
+        addWidgetListener();
+        initDataSource();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public void initDataSource() {
+        // TODO Auto-generated method stub
+        trackRDataBase = new TrackRDataBase(this);
+        mDb = trackRDataBase.getWritDatabase();
+        if(UserDataTable.getInstance().countRecord(mDb)!=0){
+            curUserDataBean = UserDataTable.getInstance().query(mDb);
+        }
+        tvtitle.setText(getResources().getString(R.string.alarttime_title));
+        wvAlarTime.setAdapter(new NumericWheelAdapter(0, 60));
+        wvAlarTime.setLabel("  " +getResources().getString(R.string.alarttime_second));
+        wvAlarTime.setVisibleItems(5);
+        Intent intent = getIntent();
+        String va = intent.getStringExtra(AlartTime);
+        if (va == null || va.equals("")) {
+            tvAlarttime.setText(getResources().getString(R.string.alarttime_tip)+"3" + getResources().getString(R.string.alarttime_second));
+            wvAlarTime.setCurrentItem(0);
+        } else {
+            tvAlarttime.setText(getResources().getString(R.string.alarttime_tip)+va + getResources().getString(R.string.alarttime_second));
+            wvAlarTime.setCurrentItem(Integer.parseInt(va));
+        }
+
+
+        OnWheelChangedListener wheelListener = new OnWheelChangedListener() {
+            public void onChanged(WheelView wheel, int oldValue, int newValue) {
+                if (!timeScrolled) {
+                    ialarttime=  wvAlarTime.getCurrentItem();
+                    tvAlarttime.setText(getResources().getString(R.string.alarttime_tip)+wvAlarTime.getCurrentItem() + getResources().getString(R.string.alarttime_second));
+                }
+            }
+        };
+        wvAlarTime.addChangingListener(wheelListener);
+        OnWheelScrollListener scrollListener = new OnWheelScrollListener() {
+            public void onScrollingStarted(WheelView wheel) {
+                timeScrolled = true;
+            }
+
+            public void onScrollingFinished(WheelView wheel) {
+                timeScrolled = false;
+                ialarttime=  wvAlarTime.getCurrentItem();
+                tvAlarttime.setText(getResources().getString(R.string.alarttime_tip)+wvAlarTime.getCurrentItem() + getResources().getString(R.string.alarttime_second));
+            }
+        };
+        wvAlarTime.addScrollingListener(scrollListener);
+    }
+
+    @Override
+    public void initWidget() {
+        // TODO Auto-generated method stub
+        btmBack = (Button) findViewById(R.id.mBtnCancel);
+        btmDone = (Button) findViewById(R.id.mBtnDone);
+        tvtitle = (TextView) findViewById(R.id.mTVTitle);
+        wvAlarTime = (WheelView) findViewById(R.id.wvalartselect);
+        tvAlarttime=(TextView) findViewById(R.id.tvalarttime_tip);
+    }
+
+    @Override
+    public void initWidgetState() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void initWidgetListener() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addWidgetListener() {
+        // TODO Auto-generated method stub
+        btmBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(AlartTime.this, UserProfileActivity.class);
+                startActivity(intent);
+                AlartTime.this.finish();
+            }
+        });
+        btmDone.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                if(curUserDataBean!=null){
+                    curUserDataBean.setMalarmtime(Integer.toString(ialarttime));
+                    UserDataTable.getInstance().update(mDb,curUserDataBean);
+                }
+                Intent intent = new Intent(AlartTime.this, UserProfileActivity.class);
+                startActivity(intent);
+                AlartTime.this.finish();
+            }
+        });
+
+
+
+    }
+
+
+}
