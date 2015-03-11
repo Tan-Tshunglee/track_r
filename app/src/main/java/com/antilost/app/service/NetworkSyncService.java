e3f542e1f674254400c8c5f1421fbe8b53897eb7
Tshunglee <tanchangli111@126.com>
2015-03-11 23:10:42 +0800

    登陆之后同步用户绑定的防丢器信息

--- app/src/main/java/com/antilost/app/service/NetworkSyncService.java
+++ app/src/main/java/com/antilost/app/service/NetworkSyncService.java
@@ -6,9 +6,15 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.antilost.app.model.TrackR;
+import com.antilost.app.network.DownloadImageCommand;
 import com.antilost.app.network.FetchAllTrackRCommand;
 import com.antilost.app.prefs.PrefsManager;
+import com.antilost.app.util.CsstSHImageData;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -35,13 +41,13 @@ public class NetworkSyncService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         if(ACTION_SYNC_AFTER_LOGIN.equals(intent.getAction())) {
             Log.i(LOG_TAG, "onStartCommand get sync after login intent");
-            (new SyncThread()).start();
+            (new TrackListSyncThread()).start();
         }
         return START_NOT_STICKY;
     }
 
 
-    class SyncThread extends Thread {
+   private class TrackListSyncThread extends Thread {
         @Override
         public void run() {
             if(mPrefs.validUserLog()) {
@@ -50,7 +56,7 @@ public class NetworkSyncService extends Service {
                 fetchAllCommand.execTask();
                 HashMap<String, TrackR> trackRs = fetchAllCommand.getBoundTrackRs();
 
-                if(trackRs == null) {
+                if(trackRs == null || trackRs.isEmpty()) {
                     Log.i(LOG_TAG, "no track bound info return.");
                     return;
                 }
@@ -58,9 +64,56 @@ public class NetworkSyncService extends Service {
                 for(Map.Entry<String, TrackR> entry: entrySet) {
                     mPrefs.addTrackR(entry.getValue());
                 }
+                TrackPhotosFetcher fetcher = new TrackPhotosFetcher();
+                fetcher.start();
             } else {
                 Log.e(LOG_TAG, "will to sync data while user is not login.");
             }
         }
+    }
+
+    private class TrackPhotosFetcher extends Thread {
+        @Override
+        public void run() {
+            Set<String> ids = mPrefs.getTrackIds();
+            for(String id : ids) {
+                final DownloadImageCommand command = new DownloadImageCommand(mPrefs.getUid(), id);
+                command.execTask();
+                byte[] rawImageData = command.getRawImageData();
+                if(rawImageData != null) {
+                    Log.v(LOG_TAG, "get rawImageData length is " + rawImageData.length);
+                    saveDataToFile(rawImageData);
+                } else {
+                    Log.e(LOG_TAG, "no rawImageData return.");
+                }
+            }
+        }
+    }
+
+    private void saveDataToFile(byte[] rawImageData, String address) {
+        File folder = ensureIconFolder();
+        File iconFile = new File(folder, address);
+        try {
+            FileOutputStream out = new FileOutputStream(iconFile);
+            out.write(rawImageData);
+            out.close();
+
+            Log.i(LOG_TAG, "saveDataToFile finish successfull .");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private File ensureIconFolder() {
+        File folder = new File(CsstSHImageData.TRACKR_IMAGE_FOLDER);
+        if(folder.exists() && folder.isFile()) {
+            folder.delete();
+            folder.mkdir();
+        } else if(!folder.exists()) {
+            folder.mkdir();
+        }
+        return folder;
     }
 }
